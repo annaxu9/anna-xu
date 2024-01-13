610fcf0cbb22147eae42006d2cadfde6db0b2e11
Anna Xu <77125837+annaxu9@users.noreply.github.com>
2024-01-13 00:51:41 -0500

    apost

--- pages/resume.js
+++ pages/resume.js
@@ -74,14 +74,14 @@ export default function Resume() {
                 <h1 className='font-bold'>Co-creator of YIMS, an iPhone and Android compatible app for Yale Intramural sports</h1>
                 <ul className='ml-12 list-disc'>
                     <li>I created all wireframes in Figma, implemented screens using React Native, connected frontend to application tier using Flask and Fetch API, and contributed to backend database design through discussions with IM Managers.</li>
-                    <li>Every year Yale’s 14 college compete in Intramural (IM) sports for the Tyng Cup. Still, there is no easy way to access and view the college standings and schedule for IM sports making it so that less students participate in this fun and awesome competition. We created YIMS to solve this problem!</li>
+                    <li>Every year Yale&apos;s 14 college compete in Intramural (IM) sports for the Tyng Cup. Still, there is no easy way to access and view the college standings and schedule for IM sports making it so that less students participate in this fun and awesome competition. We created YIMS to solve this problem!</li>
                     <li>Below is a demo where we walk through the login process, leaderboard page, how IM Managers can schedule and score games, and how students can add games to their calendar and view past game records.</li>
                 </ul>
                 <video controls width="100%" className="mx-auto m-4" style={{maxWidth: "500px"}} src="/demos/yims.mp4"></video>
                 <Link className='font-bold' href="/vibe-check">Creater of <span className='css-rainbow-text'>vibe check</span></Link>
                 <ul className='ml-12 list-disc'>
-                    <li>I created vibe check, a web application that uses the Spotify API to search for songs/artists and display their data. I also accessed and displayed each song's vibe based off some observations I made about the song.</li>
-                    <li>I leveraged Next.js' Pages API to streamline fetch calls and seamlessly handle Spotify authentication and routing within my web application. The frontend work was done using React and Tailwind.</li>
+                    <li>I created vibe check, a web application that uses the Spotify API to search for songs/artists and display their data. I also accessed and displayed each song&apos;s vibe based off some observations I made about the song.</li>
+                    <li>I leveraged Next.js&apos; Pages API to streamline fetch calls and seamlessly handle Spotify authentication and routing within my web application. The frontend work was done using React and Tailwind.</li>
                     <li>Below is a demo! And read more about vibe check <Link href="/about-vibe-check" className='underline'>here.</Link></li>
                 </ul>
                 <video controls width="100%" className="mx-auto m-4 mb-10" style={{maxWidth: "500px"}} src="/demos/vibe-check.mp4"></video>
