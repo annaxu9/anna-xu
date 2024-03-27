94cb81940c29ac84a7969638d42c691a5563187b
Anna Xu <annaxu@Annas-MacBook-Pro-2.local>
2024-03-27 14:35:42 -0400

    slight change

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <div className="sine-wave-background flex justify-center items-start w-full h-full pt-5">
         <div className="w-4/5 ">
-          <h1 className="text-center mb-10 wobble-text">Aspiring Fullstack Engineer</h1>
+          <h1 className="text-center mb-10 wobble-text">Aspiring Designer</h1>
           <div className="pb-12 flex justify-around items-center flex-col md:flex-row">
             <div className="hidden xs:block"><MyGrid className="w-2/3"/></div>
             <div className="xs:w-1/3 flex items-center xs:mt-10 md:mt-0 md:ml-24">
@@ -29,7 +29,7 @@ export default function Home() {
       <div className="py-5">
         <div className="w-3/4 mx-auto p-5">
           <h1 className="mb-4 text-center"></h1>
-          <p className=""><span className="css-rainbow-text grow-on-hover">Creative</span>, collaborative, and goal-oriented student working toward a career in software engineering, with a keen interest in advancing research in <span className="font-bold grow-on-hover ">Human-Computer Interaction</span>, to create platforms that are user friendly, well-designed, and <span className="font-bold grow-on-hover ">impact society positively</span>. Anna is qualified by her years of coding experience, her number of quality projects, and through teaching others how to code.</p>
+          <p className=""><span className="css-rainbow-text grow-on-hover">Creative</span>, collaborative, and goal-oriented student interested in advancing research in <span className="font-bold grow-on-hover ">Human-Computer Interaction</span>, to create platforms that are user friendly, well-designed, and <span className="font-bold grow-on-hover ">impact society positively</span>. Anna is qualified by her years of coding experience, her number of quality projects, and through teaching others how to code.</p>
         </div>
       </div>
       <div className="flex justify-center mb-10">
