8969f4e5b27fa27e2a22868840e82542246b8d6d
Anna Xu <anna.xu@yale.edu>
2024-08-04 18:58:47 -0500

    height

--- pages/vibe-check/index.js
+++ pages/vibe-check/index.js
@@ -72,7 +72,7 @@ export default function VibeCheck() {
         
         {/* <h1 className="text-center italic">⚠️Important Disclaimer: this app is currently seeking the approval of Spotify to be public, so it won&apos;t work without your email in my system. Email anna.xu@yale.edu if you want to use this!⚠️</h1> */}
         <Link className="bg-melrose text-white" href="/about-vibe-check">About Vibe Check</Link>
-        <div className="h-max">does this work</div>
+        <div className="h-full">does this work</div>
       </div>
     </AuthProvider>
   );
